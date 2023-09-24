24f6ba40bfc522beaf65012720d5158188fdc122
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2023-09-24 22:02:39 +0300

    Add project files.

---
+++ 2_Conditional Statements_Nested Conditional statements - Exercise/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace _2_Conditional_Statements_Nested_Conditional_statements___Exercise;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
